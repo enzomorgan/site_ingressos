43df0404be60b3d731fa119cdca2d539306300f9
enzomorgan <enzomelo1508@gmail.com>
2026-05-18 08:14:43 -0300

    novas configurações

--- ticket_platform/ticket_platform/settings.py
+++ ticket_platform/ticket_platform/settings.py
@@ -21,11 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-aidnnt2lc^!f44jk$()rr9#lptdkui^k^yyn)tmo4cg#64xf!z'
+SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
+ALLOWED_HOSTS = config(
+    'ALLOWED_HOSTS',
+    default='127.0.0.1,localhost',
+    cast=lambda value: [host.strip() for host in value.split(',')]
+)
 ALLOWED_HOSTS = []
 
 
